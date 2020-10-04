9090b8deb138310a3644293fe1f9d3bc5a8e5c11
MelindaJoArmbruster <melinda@armbrustermail.com>
2020-10-04 14:22:32 -0500

    completed function 05 assignments 1 and 2

--- bootcamp-prep-master/pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ bootcamp-prep-master/pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+//Define a function, `defaultGreet`, that takes a first and a last name.
+//`defaultGreet` should return a string greeting for the given names. If the user
+//does not define a last name, assume the last name is 'Doe'.
+function defaultGreet (firstName, lastName) {
+    if (!lastName) lastName = "Doe";
+    return "Hi " + firstName + " " + lastName + "!";
+}
+
