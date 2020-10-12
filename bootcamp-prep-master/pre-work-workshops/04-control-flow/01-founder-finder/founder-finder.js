45afc1d916aee899255478c8f9f2fda55140bb49
MelindaJoArmbruster <melinda@armbrustermail.com>
2020-10-12 09:40:49 -0500

    completed flow control 3 problems

--- bootcamp-prep-master/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ bootcamp-prep-master/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,12 @@
 let name = 'David';
 // let name = 'Nimit';
 // let name = 'Someone else';
-
-let found;
-
 // YOUR CODE BELOW
+let found;
+if (name === 'David') {
+    found = true;
+} else if (name === 'Nimit') {
+    found = true;
+} else {
+    found = false;
+}
