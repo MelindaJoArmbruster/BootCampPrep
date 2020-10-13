74a4b071fc4e73064aa8c5bd8c63d60e7e7c208e
MelindaJoArmbruster <melinda@armbrustermail.com>
2020-10-13 09:42:03 -0500

    worked problems 02 thru 05 of practice set

--- bootcamp-prep-master/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ bootcamp-prep-master/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+let str = "";
+function doYouPlayTheTheremin (str) {
+    if ((str[0] === "s") || (str[0] === "S")) {
+        return true;
+    } else {
+        return false;
+    }
+} 
--- bootcamp-prep-master/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ bootcamp-prep-master/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,4 @@
 // YOUR CODE BELOW
+const maxOfThree = (num1,num2,num3) => {
+    return Math.max(num1,num2,num3);
+}
