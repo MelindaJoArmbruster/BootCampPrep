a48204b2645e540e02117868a77d446edd97156a
MelindaJoArmbruster <melinda@armbrustermail.com>
2020-11-04 20:14:11 -0600

    refactored the coercion and truthiness problems during class

--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,12 +1,12 @@
 function myOr(arg1, arg2, arg3) {
-    if (arg1 != false && arg1 != null) return arg1;
-    else if (arg2 != false && arg2 != null) return arg2;
-    else if (arg3 != false && arg3 != null) return arg3;
+    if (arg1) return arg1;
+    else if (arg2) return arg2;
+    else if (arg3) return arg3;
     else return arg3;
 }
 function myAnd(arg1, arg2, arg3) {
-    if (arg1 == false || arg1 === null) return arg1;
-    else if (arg2 == false || arg2 === null) return arg2;
-    else if (arg3 == false || arg3 === null) return arg3;
+    if (!arg1) return arg1;
+    else if (!arg2) return arg2;
+    else if (!arg3) return arg3;
     else return arg3;
 }
