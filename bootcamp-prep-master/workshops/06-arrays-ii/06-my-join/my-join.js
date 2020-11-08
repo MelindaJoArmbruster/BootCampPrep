185eed3a503b1836b4c40970295bb343549ac262
MelindaJoArmbruster <melinda@armbrustermail.com>
2020-11-08 14:30:11 -0600

    finished arrays II problems

--- bootcamp-prep-master/workshops/06-arrays-ii/06-my-join/my-join.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,8 @@
-// YOUR CODE BELOW
+function myJoin(arr, separator = ',') {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str += !arr[i] ? '' : arr[i];
+        if (i != arr.length -1) str += separator;
+    }
+    return str;
+}
--- bootcamp-prep-master/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,16 @@
-// YOUR CODE BELOW
+function rotateArray(originalArray, rotateNum) {
+    let rotated = [];
+    if (rotateNum === 0) {
+        return originalArray;
+    } else if (rotateNum > 0) {
+        let begin = originalArray.slice(-rotateNum);
+        let end = originalArray.slice(0,(originalArray.length -rotateNum));
+        rotated = begin.concat(end);
+    } else {
+        let begin = originalArray.slice(-1*rotateNum);
+        let end = originalArray.slice(0,(-rotateNum));
+        rotated = begin.concat(end);
+    }
+    return rotated;
+}
+//rotateArray(['first', 'second', 'third', 'fourth'], 1);
