1f8329353d28811da691a36f8d71b9ad5a3627b3
MelindaJoArmbruster <melinda@armbrustermail.com>
2020-11-09 14:08:32 -0600

    completed first problem in objects lesson

--- bootcamp-prep-master/workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ bootcamp-prep-master/workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,33 @@
-// YOUR CODE BELOW
+/*let transactions = [
+  {
+    name: "Tons of glitter",
+    amount: 70,
+  },
+  {
+    name: "Porcelain Pink Flamingos",
+    amount: 92,
+  },
+  {
+    name: "Chandelier replacement",
+    amount: 10000,
+  },
+  {
+    name: "Dinner at TGIF x6",
+    amount: 350,
+  },
+];
+*/
+//sumTransactions(transactions)  => 10512
+
+function lastFridayNight(arr) {
+  let sum = 0;
+  if (!Array.isArray(arr)) {
+    // added this to handle the test case which passed in non-array
+  } else {
+    sum = arr.reduce((accum, current) => accum + current.amount, 0);
+  }
+  console.log(arr.length);
+  return sum;
+}
+
+//console.log(lastFridayNight(transactions));
