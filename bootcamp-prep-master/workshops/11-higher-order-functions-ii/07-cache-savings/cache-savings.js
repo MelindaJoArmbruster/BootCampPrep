172e9f8b90bd6427d7fb7704ca702e59b4357239
MelindaJoArmbruster <melinda@armbrustermail.com>
2020-12-02 08:55:29 -0600

    finished last problem in higher order functions II

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function cacheSavings(callback) {
+  let results = {};
+  return function (arg) {
+    if (!(arg in results)) {
+      let callbackResult = callback(arg);
+      results[arg] = callbackResult;
+    }
+    return results[arg];
+  };
+}
